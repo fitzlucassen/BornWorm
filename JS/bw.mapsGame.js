86848b73a9a6df5e14d7fba3a1ceb3044df26338
Titou <thibault.dulon@gmail.com>
2013-09-12 21:36:47 +0200

    dev marker maps with fb

--- JS/bw.mapsGame.js
+++ JS/bw.mapsGame.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     },1000);
     
     $('#layout').on('click', '.nbFriends', function(){
-	initializeMaps();
+	initializeMaps(Facebook.response);
     });
 });
 	
